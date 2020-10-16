ffb0cab77e90ab7bd551f44090f40a086df5c651
Harshit-Kushwah <rahulkumar227659@gmail.com>
2020-10-16 15:01:33 +0530

    pp

--- server.js
+++ server.js
@@ -29,26 +29,22 @@ con.connect((err) => {
         console.log("sss")
 })
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static("home/build"))
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
-app.get('/get', (req, res) => {
-    if (req) {
-        con.query(qu.getdt1, (err, nex) => {
-            if (!err) {
-                res.send(nex)
-            } else
-                res.send("Error")
-        })
-    } else
-        console.log("Select Comand Error")
-})
 app.get("/",(req,res)=>{
-   if(req){
-      con.query(qu.getdt1,(err,nex)=>{
-          res.send(nex)
-      }) 
-   }
+      app.use(express.static("client/build"))
+//    if(req){
+//       con.query(qu.getdt1,(err,nex)=>{
+//           res.send(nex)
+//       }) 
+//    }
 })
 app.get('/get/:id', (req, res) => {
     if (req) {
